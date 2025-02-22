70f9c704758e92db89253d65631de7e1b40f94ab
zhangqi <dto-zhangqi@renruihr.com>
2025-02-22 20:08:23 +0800

    fix: 补充一键部署

---
+++ docker-deploy.sh
@@ -0,0 +1,16 @@
+cd ./server
+docker build -t registry.cn-hangzhou.aliyuncs.com/one-registry/docker-swarm-server .
+docker push registry.cn-hangzhou.aliyuncs.com/one-registry/docker-swarm-server
+
+cd ..
+cd ./user
+docker build -t registry.cn-hangzhou.aliyuncs.com/one-registry/docker-swarm-user .
+docker push registry.cn-hangzhou.aliyuncs.com/one-registry/docker-swarm-user
+
+cd ..
+cd ./case
+docker build -t registry.cn-hangzhou.aliyuncs.com/one-registry/docker-swarm-case .
+docker push registry.cn-hangzhou.aliyuncs.com/one-registry/docker-swarm-case
+
+cd ..
+docker stack deploy -c docker-compose.yaml my-custer
