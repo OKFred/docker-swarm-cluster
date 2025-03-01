879a42e7d4c22ca276d24ce58c90a492b5cb14cd
devfred <623413520@qq.com>
2025-03-01 23:42:43 +0800

    fix: 启用devtools

--- platform/vite.config.ts
+++ platform/vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import vueDevTools from "vite-plugin-vue-devtools";
 import path from "path";
+
 const pathSrc = path.resolve(__dirname, "src");
+
 // https://vite.dev/config/
 export default defineConfig(() => {
-    return {
-        resolve: {
-            alias: {
-                "@": pathSrc,
-            },
-        },
-        server: {
-            host: "0.0.0.0",
-        },
-        plugins: [vue()],
-    };
+  return {
+    resolve: {
+      alias: {
+        "@": pathSrc,
+      },
+    },
+    server: {
+      host: "0.0.0.0",
+    },
+    plugins: [vue(), vueDevTools()],
+  };
 });
