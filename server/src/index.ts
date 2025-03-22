539af587647cfdd739c8f09aecbd74ce688abe4c
okfred <zq.admin.vip@gmail.com>
2025-03-23 00:51:18 +0800

    fix: 类型补充

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import errorHandler from "@/middleware/errorHandler/index";
 // import pathHandler from "@/middlewares/pathHandler";
 // import normalRouter from "@/routes/normalRouter";
 
-import type { AppBindings } from "@/types/app.d";
+import type { AppBindings, NodeHonoContext } from "@/types/app.d";
 import corsHandler from "@/middleware/cors/index";
 // import basicAuthHandler from "@/middlewares/auth/basic";
 import docRegister from "@/doc/index";
@@ -12,20 +12,22 @@ import logger from "@/middleware/logger/index";
 import nodeServer from "@/middleware/nodeServer/index";
 import routeRegister from "@/api/index";
 async function createApp() {
-    const app = new OpenAPIHono<AppBindings>();
-    app.get("/", (c) => c.json({ ok: true, message: new Date().toLocaleString() }));
-    // loggerRegister(app);
-    errorHandler(app);
-    corsHandler(app);
-    logger(app);
-    // basicAuthHandler(app);
-    // bearerAuthHandler(app);
-    // pathHandler(app);
-    docRegister(app);
-    // normalRouter(app);
-    await routeRegister(app);
-    nodeServer(app);
-    return app;
+  const app = new OpenAPIHono<AppBindings>();
+  app.get("/", (c: NodeHonoContext) =>
+    c.json({ ok: true, message: new Date().toLocaleString() })
+  );
+  // loggerRegister(app);
+  errorHandler(app);
+  corsHandler(app);
+  logger(app);
+  // basicAuthHandler(app);
+  // bearerAuthHandler(app);
+  // pathHandler(app);
+  docRegister(app);
+  // normalRouter(app);
+  await routeRegister(app);
+  nodeServer(app);
+  return app;
 }
 
 const app = createApp();
